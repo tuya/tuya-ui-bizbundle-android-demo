7f2efca11090af565ec76b353a2f18752cdc7890
liya.luo@tuya.com <liya.luo@tuya.com>
2022-03-01 16:12:09 +0800

    update 3.34.5

--- devicedetail/src/main/java/com/tuya/smart/bizubundle/panelmore/demo/PanelMoreExpandServiceImpl.java
+++ devicedetail/src/main/java/com/tuya/smart/bizubundle/panelmore/demo/PanelMoreExpandServiceImpl.java
@@ -1,80 +1,80 @@
-package com.tuya.smart.bizubundle.panelmore.demo;
-
-import android.content.Context;
-import android.os.Handler;
-import android.text.TextUtils;
-import android.widget.Toast;
-
-import com.tuya.smart.panel.usecase.panelmore.bean.IMenuBean;
-import com.tuya.smart.panel.usecase.panelmore.service.AbsPanelMoreExpandService;
-import com.tuya.smart.panel.usecase.panelmore.service.IMenuItemCallback;
-import com.tuya.smart.sdk.bean.DeviceBean;
-import com.tuya.smart.sdk.bean.GroupBean;
-import com.tuya.smart.uispec.list.plug.text.TextBean;
-
-public class PanelMoreExpandServiceImpl extends AbsPanelMoreExpandService {
-
-    public PanelMoreExpandServiceImpl() {
-        super();
-    }
-
-    @Override
-    public void devClickItem(Context context, int action, DeviceBean deviceBean, boolean isAdmin) {
-
-    }
-
-    @Override
-    public IMenuBean insertDevMenuItem(Context context, String type, boolean isAdmin, DeviceBean deviceBean, float order) {
-        if (isAdmin && TextUtils.equals(type, "c_test_insert")) {
-            return new IMenuBean("title", "subtitle", "1",
-                    java.lang.String.valueOf(R.id.action_test_insert),
-                    order);
-        }
-        return null;
-    }
-
-    @Override
-    public void insertDevMenuItemAsync(Context context, String type, boolean isAdmin, DeviceBean deviceBean, float order, IMenuItemCallback callback) {
-        if (isAdmin && TextUtils.equals(type, "c_test_async_insert")) {
-            Handler handler = new Handler();
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    callback.setMenuItem(new IMenuBean("async_title", "async_subtitle", "1",
-                            java.lang.String.valueOf(R.id.action_test_async_insert),
-                            order));
-                }
-            }, 1000 * 5);
-        }
-    }
-
-    @Override
-    public void groupClickItem(Context context, int action, GroupBean groupBean, boolean isAdmin) {
-
-    }
-
-    @Override
-    public IMenuBean insertGroupMenuItem(Context context, String type, boolean isAdmin, GroupBean deviceBean, float order) {
-        if (isAdmin && TextUtils.equals(type, "c_test_insert")) {
-            return new IMenuBean("title", "subtitle", "1",
-                    java.lang.String.valueOf(R.id.action_test_insert),
-                    order);
-        }
-        return null;
-    }
-
-    @Override
-    public void insertGroupMenuItemAsync(Context context, String type, boolean isAdmin, GroupBean deviceBean, float order, IMenuItemCallback callback) {
-        if (isAdmin && TextUtils.equals(type, "c_test_async_insert")) {
-            Handler handler = new Handler();
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    callback.setMenuItem(new IMenuBean("async_title", "async_subtitle", "1",
-                            java.lang.String.valueOf(R.id.action_test_async_insert),
-                            order));
-                }
-            }, 1000 * 5);
-        }
-    }
-}
+//package com.tuya.smart.bizubundle.panelmore.demo;
+//
+//import android.content.Context;
+//import android.os.Handler;
+//import android.text.TextUtils;
+//import android.widget.Toast;
+//
+//import com.tuya.smart.panel.usecase.panelmore.bean.IMenuBean;
+//import com.tuya.smart.panel.usecase.panelmore.service.AbsPanelMoreExpandService;
+//import com.tuya.smart.panel.usecase.panelmore.service.IMenuItemCallback;
+//import com.tuya.smart.sdk.bean.DeviceBean;
+//import com.tuya.smart.sdk.bean.GroupBean;
+//import com.tuya.smart.uispec.list.plug.text.TextBean;
+//
+//public class PanelMoreExpandServiceImpl extends AbsPanelMoreExpandService {
+//
+//    public PanelMoreExpandServiceImpl() {
+//        super();
+//    }
+//
+//    @Override
+//    public void devClickItem(Context context, int action, DeviceBean deviceBean, boolean isAdmin) {
+//
+//    }
+//
+//    @Override
+//    public IMenuBean insertDevMenuItem(Context context, String type, boolean isAdmin, DeviceBean deviceBean, float order) {
+//        if (isAdmin && TextUtils.equals(type, "c_test_insert")) {
+//            return new IMenuBean("title", "subtitle", "1",
+//                    java.lang.String.valueOf(R.id.action_test_insert),
+//                    order);
+//        }
+//        return null;
+//    }
+//
+//    @Override
+//    public void insertDevMenuItemAsync(Context context, String type, boolean isAdmin, DeviceBean deviceBean, float order, IMenuItemCallback callback) {
+//        if (isAdmin && TextUtils.equals(type, "c_test_async_insert")) {
+//            Handler handler = new Handler();
+//            handler.postDelayed(new Runnable() {
+//                @Override
+//                public void run() {
+//                    callback.setMenuItem(new IMenuBean("async_title", "async_subtitle", "1",
+//                            java.lang.String.valueOf(R.id.action_test_async_insert),
+//                            order));
+//                }
+//            }, 1000 * 5);
+//        }
+//    }
+//
+//    @Override
+//    public void groupClickItem(Context context, int action, GroupBean groupBean, boolean isAdmin) {
+//
+//    }
+//
+//    @Override
+//    public IMenuBean insertGroupMenuItem(Context context, String type, boolean isAdmin, GroupBean deviceBean, float order) {
+//        if (isAdmin && TextUtils.equals(type, "c_test_insert")) {
+//            return new IMenuBean("title", "subtitle", "1",
+//                    java.lang.String.valueOf(R.id.action_test_insert),
+//                    order);
+//        }
+//        return null;
+//    }
+//
+//    @Override
+//    public void insertGroupMenuItemAsync(Context context, String type, boolean isAdmin, GroupBean deviceBean, float order, IMenuItemCallback callback) {
+//        if (isAdmin && TextUtils.equals(type, "c_test_async_insert")) {
+//            Handler handler = new Handler();
+//            handler.postDelayed(new Runnable() {
+//                @Override
+//                public void run() {
+//                    callback.setMenuItem(new IMenuBean("async_title", "async_subtitle", "1",
+//                            java.lang.String.valueOf(R.id.action_test_async_insert),
+//                            order));
+//                }
+//            }, 1000 * 5);
+//        }
+//    }
+//}
